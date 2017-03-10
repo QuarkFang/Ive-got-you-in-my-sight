2724f90551e9818a8add5638b5c81d09851084a1
JamesQFreeman <ws.bond1@gmail.com>
2017-03-11 02:21:11 +0800

    finished the gravity center finding

--- real_end/dotCalculation/dotCalculation.go
+++ real_end/dotCalculation/dotCalculation.go
@@ -7,26 +7,73 @@ import (
 	"image/color"
 )
 
-func drawHorizontalLine(x1, y, x2 int, col color.Color, img *(image.RGBA)) {
+func getDxDy(img *image.RGBA) (int, int) {
+	Dx := img.Bounds().Dx()
+	Dy := img.Bounds().Dy()
+	return Dx, Dy
+}
+
+// LeaveColor : all the col in the img will be repalced with white
+// then all the other color will be repalced by black, it is for test only
+// usage : for test only, to test whther it is possible to use the method
+func LeaveColor(col color.Color, img *image.RGBA) {
+	Dx, Dy := getDxDy(img)
+	for x := 0; x < Dx; x++ {
+		for y := 0; y < Dy; y++ {
+			if img.At(x, y) != col {
+				img.SetRGBA(x, y, color.RGBA{0, 0, 0, 255})
+			} else {
+				img.SetRGBA(x, y, color.RGBA{255, 255, 255, 255})
+			}
+		}
+	}
+}
+
+// CalcuGravityCenter : calcuGravityCenter of col in the image
+func CalcuGravityCenter(col color.Color, img *image.RGBA) (int, int) {
+	Dx, Dy := getDxDy(img)
+	var sumX, sumY, sumNum int = 0, 0, 0
+	for x := 0; x < Dx; x++ {
+		for y := 0; y < Dy; y++ {
+			if img.At(x, y) == col {
+				sumX += x
+				sumY += y
+				sumNum++
+			}
+		}
+	}
+	println(sumX / sumNum)
+	println(sumY / sumNum)
+
+	return sumX / sumNum, sumY / sumNum
+}
+
+func drawHorizontalLine(x1, y, x2 int, col color.Color, img *image.RGBA) {
 	//it's easy
 	for ; x1 <= x2; x1++ {
 		img.Set(x1, y, col)
 	}
 }
 
-func drawVerticalLine(x, y1, y2 int, col color.Color, img *(image.RGBA)) {
+func drawVerticalLine(x, y1, y2 int, col color.Color, img *image.RGBA) {
 	for ; y1 <= y2; y1++ {
 		img.Set(x, y1, col)
 	}
 }
 
-func drawCross(x, y int, col color.Color, img *(image.RGBA)) {
-	drawHorizontalLine(x-5, y, x+5, col, img)
-	drawVerticalLine(x, y-5, y+5, col, img)
+func drawCross(x, y int, col color.Color, img *image.RGBA) {
+	drawHorizontalLine(x-10, y, x+10, col, img)
+	drawHorizontalLine(x-10, y+1, x+10, col, img)
+	drawHorizontalLine(x-10, y-1, x+10, col, img)
+
+	drawVerticalLine(x, y-10, y+10, col, img)
+
+	drawVerticalLine(x-1, y-10, y+10, col, img)
+	drawVerticalLine(x+1, y-10, y+10, col, img)
 }
 
 // DrawBlueMark : It draw a blue mark (0,0,255) at (x,y) in the RBGA image img)
 func DrawBlueMark(x, y int, img *(image.RGBA)) {
-	blue := color.RGBA{0, 0, 255, 255}
-	drawCross(100, 100, blue, img)
+	blue := color.RGBA{0, 0, 0, 255}
+	drawCross(x, y, blue, img)
 }
--- real_end/main.go
+++ real_end/main.go
@@ -4,21 +4,31 @@ import(
 	"./dotCalculation"
 	"image"
 	"os"
-	"image/jpeg"
+	"image/draw"
+	"image/png"
 )
 
 func save(img *(image.RGBA), filename string){
 	imgw,_:=os.Create(filename)
-	jpeg.Encode(imgw,img,&jpeg.Options{jpeg.DefaultQuality})
+	png.Encode(imgw,img)
 }
 
 func main(){
-	screen_size := image.Rect(0,0,200,200)
-	test_img :=image.NewRGBA(screen_size)
+	imgb,_:=os.Open("Untitled.png")
+	GCtest,_:=png.Decode(imgb)
+	defer imgb.Close()
+	bound := GCtest.Bounds()
+	GCtestRGBA:=image.NewRGBA(bound)
+	draw.Draw(GCtestRGBA,bound,GCtest,bound.Min,draw.Src)
+	col := GCtestRGBA.At(0,0)
 	
-	save(test_img,"fuck.jpeg")
-	dotCalculation.DrawBlueMark(200,200,test_img)
-	save(test_img,"fuck2.jpeg")
-	//save it
+	save(GCtestRGBA,"fuck.png")
+	//dotCalculation.LeaveColor(col,GCtestRGBA)
+	save(GCtestRGBA,"fuck2.png")
+	
+	px,py:=dotCalculation.CalcuGravityCenter(col,GCtestRGBA)
+	dotCalculation.DrawBlueMark(px,py,GCtestRGBA)
+	save(GCtestRGBA,"fuck3.png")
+
 }
 
